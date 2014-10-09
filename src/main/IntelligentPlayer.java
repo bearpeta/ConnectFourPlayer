bb63e5e7eb5c3e580e8fa905bd0ed2bb33d55925
alessandro.petrillo@stud.hslu.ch <alessandro.petrillo@stud.hslu.ch>
2014-10-09 18:31:28 +0200

    Add intelligentPlayer class

---
+++ src/main/IntelligentPlayer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+import ch.hslu.ai.connect4.Player;
+
+/**
+ *
+ * @author Alessandro
+ */
+public class IntelligentPlayer extends Player {
+
+    public IntelligentPlayer(String name, char symbol) {
+        super(name, symbol);
+    }
+
+    @Override
+    public int play(char[][] board) {
+        int column = -1;
+        
+        return column
+    }
+    
+}

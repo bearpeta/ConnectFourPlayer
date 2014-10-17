3e7423f8575d766fbdfeca660b53a3e4db0e30a4
alessandro.petrillo@stud.hslu.ch <alessandro.petrillo@stud.hslu.ch>
2014-10-17 15:32:09 +0200

    for pull

--- src/main/IntelligentPlayer.java
+++ src/main/IntelligentPlayer.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package main;
 
 import ch.hslu.ai.connect4.Player;
@@ -13,47 +12,49 @@ import ch.hslu.ai.connect4.Player;
  * @author Alessandro
  */
 public class IntelligentPlayer extends Player {
-    
+
     private boolean firstMoveover;
 
     public IntelligentPlayer(String name, char symbol) {
         super(name, symbol);
-        firstMoveover =false;
+        firstMoveover = false;
     }
 
-     /**
+    /**
      * The following method allows you to implement your own game intelligence.
-     * The method must return the column number where the computer player puts the next disc. 
-     * board[i][j] = cell content at position (i,j), i = column, j = row
-     * 
-     * If board[i][j] = this.getSymbol(), the cell contains one of your discs
-     * If board[i][j] = '-', the cell is empty
-     * Otherwise, the cell contains one of your opponent's discs
+     * The method must return the column number where the computer player puts
+     * the next disc. board[i][j] = cell content at position (i,j), i = column,
+     * j = row
+     *
+     * If board[i][j] = this.getSymbol(), the cell contains one of your discs If
+     * board[i][j] = '-', the cell is empty Otherwise, the cell contains one of
+     * your opponent's discs
+     *
      * @param board The current game board
      * @return The columns number where you want to put your disc
      */
     @Override
     public int play(char[][] board) {
         int column = -1;
-        if(!firstMoveover && checkIfFirstMove(board)){
+        if (!firstMoveover && checkIfFirstMove(board)) {
             firstMoveover = true;
-            return((board.length-1)/2);
+            return ((board.length - 1) / 2);
         }
-        
+
         do {
-    		column = (int)(Math.random() * board.length);
-    	} while(board[column][0] != '-');
-        
+            column = (int) (Math.random() * board.length);
+        } while (board[column][0] != '-');
+
         return column;
     }
-    
-    public boolean checkIfFirstMove(char[][] board){
-        for(int i = 0; i < board.length-1; i++){
-            if(board[i][board[0].length-1]!='-'){
+
+    public boolean checkIfFirstMove(char[][] board) {
+        for (int i = 0; i < board.length - 1; i++) {
+            if (board[i][board[0].length - 1] != '-') {
                 return false;
             }
         }
         return true;
     }
-    
+
 }
